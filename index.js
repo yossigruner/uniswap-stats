e471bf6937770ee17c95baefd46235207190aa44
Yossi Gruner <yossigruner@gmail.com>
2021-04-05 22:37:34 -0600

    Script Update

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
 const axios = require('axios')
 
-const MIN_DAILY_VOLUME_USD = 1000000;
-const DATE = 1617392958 
-const MULTIPLIER = 3;
+const MIN_DAILY_VOLUME_USD = 100000;
+const MIN_DAILY_LIQUIDITY_USD = 100;
+
+
+const MULTIPLIER = 2;
+const NUMBEROFDAYS = 1;
+const DATE = Math.round(Date.now() / 1000 - (86400 * NUMBEROFDAYS))
+
 
 const main = async () => {
-
-    // get pairs
-    const res = await axios.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', {
-        query: `
-        {
-            pairDayDatas(first: 100, orderBy: date, orderDirection: asc,
-              where: {
-                dailyVolumeUSD_gt: ` + MIN_DAILY_VOLUME_USD + `
-                date_gt: ` + DATE + `
-              })
-              {
-                token0 {
-                    name
-                },
-                token1{
-                    name
-                }
-                dailyTxns
-                dailyVolumeUSD
-                reserveUSD
-                pairAddress,
-                date
-            }
-           }
-    `
-    })
+    let skip = 0;
+    let totalPairs = 0;
 
     // Step #1
     // filter muliplier
     var filterResult = []
-    for (const pairDay of res.data.data.pairDayDatas) {
-        if (pairDay.dailyVolumeUSD > (MULTIPLIER * pairDay.reserveUSD)) {
-            filterResult.push(pairDay);
+    let moreResults = true;
+    while (moreResults) {
+        // get pairs
+        console.log("calling with skip = " + skip);
+        const res = await axios.post('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', {
+            query: `
+            {
+                    pairDayDatas(first: 1000,skip: `+ skip + `,  orderBy: date, orderDirection: asc,skip: $skip,
+                    where: {
+                        dailyVolumeUSD_gt: ` + MIN_DAILY_VOLUME_USD + `
+                        reserveUSD_gt: ` + MIN_DAILY_LIQUIDITY_USD + `
+                        date_gt: ` + DATE + `
+                    })
+                    {
+                        token0 {
+                            name
+                        },
+                        token1{
+                            name
+                        }
+                        dailyTxns
+                        dailyVolumeUSD
+                        reserveUSD
+                        pairAddress,
+                        date
+                    }
+                }
+            
+        `
+        })
+
+        if (res.data.data != null && res.data.data.pairDayDatas != null) {
+            skip += res.data.data.pairDayDatas.length;
+            totalPairs += res.data.data.pairDayDatas.length;
         }
+
+        for (const pairDay of res.data.data.pairDayDatas) {
+            if (pairDay.dailyVolumeUSD > (MULTIPLIER * pairDay.reserveUSD)) {
+                filterResult.push(pairDay);
+            }
+        }
+
+        if (res.data.data.pairDayDatas == null || res.data.data.pairDayDatas.length < 1000) {
+            moreResults = false;
+        }
+
+        console.log("Found total of pairs: " + totalPairs);
+        console.log("Found total of suitable pairs: " + filterResult.length);
+
     }
+
 
     // Setp #2
 
     for (const pairDay of filterResult) {
-        
+
     }
 
 
     // Step #3
 
     // Step #4
-    
+
 
     return filterResult
 }
@@ -62,7 +89,6 @@ main()
         console.log("========================================================")
         console.log(res)
         console.log("========================================================")
-
 
     })
     .catch((error) => {
