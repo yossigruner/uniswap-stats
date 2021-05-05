a6f87b419cc65da78a87140fdc4041ed30d6d684
Nitsan Lavie <nitsanf4@gmail.com>
2021-05-05 22:54:27 +0300

    Chaning config param

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const MIN_DAILY_VOLUME_USD = 500000;
 const MIN_DAILY_LIQUIDITY_USD = 100000;
 const LIQUIDITY_V3 = true;
 
-const MULTIPLIER = 15;
+const MULTIPLIER = 0.2;
 
 const TIME_INTERVALS_IN_DAYS = [1, 4/24, 2/24, 1/24 ];
 const DATES = TIME_INTERVALS_IN_DAYS.map(x=> Math.round(Date.now() / 1000 - (86400 * x)));
