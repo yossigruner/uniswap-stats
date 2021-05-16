636284fc6a20266669bc5e6c400939107ed5eed1
Nitsan Lavie <nitsanf4@gmail.com>
2021-05-16 09:59:21 +0300

    Adding consts

--- v3/index_liquidity.js
+++ v3/index_liquidity.js
@@ -3,11 +3,14 @@ const api = require('./apiv3Queries.js');
 const consts = require('./consts.js');
 const log = require('debug-level').log('test');
 
+const POOL_ID = '0x5116f278d095ec2ad3a14090fedb3e499b8b5af6';
+const MIN_PRICE = 2297;
+const MAX_PRICE = 3359;
 
 async function main() {
     const ethUsdtPool = await api.getPoolByPoolId(consts.ETH_USDT_POOL_ID);
-    const pool = await api.getPoolByPoolId('0x5116f278d095ec2ad3a14090fedb3e499b8b5af6');
-    const t = await liquidityCollector.getLiquidityInRangeInUSD(pool[0],2297,3359, ethUsdtPool[0]);
+    const pool = await api.getPoolByPoolId(POOL_ID);
+    const t = await liquidityCollector.getLiquidityInRangeInUSD(pool[0],MIN_PRICE,MAX_PRICE, ethUsdtPool[0]);
 
     return t;
 }
