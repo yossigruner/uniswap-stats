05a8bb2b66211c3430b69c0ad5d15509c1f340b2
Nitsan Lavie <nitsanf4@gmail.com>
2021-05-20 21:37:29 +0300

    Adding data to liquidity in range script

--- v3/index_liquidity.js
+++ v3/index_liquidity.js
@@ -1,21 +1,38 @@
 const liquidityCollector = require('./liquidityCollector.js');
 const api = require('./apiv3Queries.js');
 const consts = require('./consts.js');
+const pools = require('./pools.js');
 const log = require('debug-level').log('test');
 
-const POOL_ID = '0x5116f278d095ec2ad3a14090fedb3e499b8b5af6';
-const MIN_PRICE = 2297;
-const MAX_PRICE = 3359;
+
+const POOL_ID = '0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8';
+const MIN_PRICE = 3700;
+const MAX_PRICE = 4200;
 
 async function main() {
     const ethUsdtPool = await api.getPoolByPoolId(consts.ETH_USDT_POOL_ID);
     const pool = await api.getPoolByPoolId(POOL_ID);
-    const t = await liquidityCollector.getLiquidityInRangeInUSD(pool[0],MIN_PRICE,MAX_PRICE, ethUsdtPool[0]);
+    const liquidity = await liquidityCollector.getLiquidityInRangeInUSD(pool[0],MIN_PRICE,MAX_PRICE, ethUsdtPool[0]);
+    let data = {};
+    try {
+        data = await pools.proccessOnePool(pool[0], ethUsdtPool[0]);
+    } catch (e) {
+        log.warn('[index_liquidity.main] - Error in process one pool');
+        log.debug(e);
+    }
 
-    return t;
+    return {
+        liquidity,
+        data,
+    };
 }
 
 main()
     .then((res) => {
-        log.info('Liquidity in range -- ' + JSON.stringify(res));
+        log.info('Liquidity in range -- ' + JSON.stringify(res.liquidity));
+        res.data.forEach((r) => {
+            log.info('Pool by time interval (time interval, daily volume, estimated revenue) => ' +
+                '' + '(' + r.timeInterval + ' ,' + r.volumeDailyTimeRange + ', '+ ( Number(r.volumeDailyTimeRange) / Number(res.liquidity) ) * Number(r.fee) + ')');
+        });
+
     });
